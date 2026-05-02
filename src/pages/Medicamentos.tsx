7e2f801871fd0d7f220f6fa9dd859a7623bbfeac
kaynan98 <lucascoelho9821@gmail.com>
2026-05-02 01:57:33 -0300

    [AI] src/pages/Medicamentos.tsx

---
+++ src/pages/Medicamentos.tsx
@@ -0,0 +1,10 @@
+function Medicamentos() {
+  return (
+    <div>
+      <h1>Cadastro de Medicamentos</h1>
+      <p>Em breve: formulário e listagem de medicamentos com lotes.</p>
+    </div>
+  )
+}
+
+export default Medicamentos
