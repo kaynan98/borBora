7a6b2420048b9fac47f8316ea43c5a45c0cb1ded
kaynan98 <lucascoelho9821@gmail.com>
2026-05-02 02:37:03 -0300

    [AI] src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import Index from './pages/Index'
-import Clientes from './pages/Clientes'
-import Medicamentos from './pages/Medicamentos'
 import Receitas from './pages/Receitas'
 import HistoricoCliente from './pages/HistoricoCliente'
 
@@ -9,15 +7,11 @@ function App() {
   return (
     <BrowserRouter>
       <nav style={{ padding: '1rem', background: '#f0f0f0', marginBottom: '1rem' }}>
-        <Link to="/clientes" style={{ marginRight: '1rem' }}>Clientes</Link>
-        <Link to="/medicamentos" style={{ marginRight: '1rem' }}>Medicamentos</Link>
         <Link to="/receitas" style={{ marginRight: '1rem' }}>Receitas</Link>
         <Link to="/historico">Histórico do Cliente</Link>
       </nav>
       <Routes>
         <Route path="/" element={<Index />} />
-        <Route path="/clientes" element={<Clientes />} />
-        <Route path="/medicamentos" element={<Medicamentos />} />
         <Route path="/receitas" element={<Receitas />} />
         <Route path="/historico" element={<HistoricoCliente />} />
       </Routes>
