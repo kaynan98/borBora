5e2d755259fb62030d2acf9c5cfb2a741ec4379c
kaynan98 <lucascoelho9821@gmail.com>
2026-05-02 01:57:32 -0300

    [AI] src/pages/Clientes.tsx

---
+++ src/pages/Clientes.tsx
@@ -0,0 +1,10 @@
+function Clientes() {
+  return (
+    <div>
+      <h1>Cadastro de Clientes</h1>
+      <p>Em breve: formulário e listagem de clientes.</p>
+    </div>
+  )
+}
+
+export default Clientes
