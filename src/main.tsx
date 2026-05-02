034fe8f336663a589393729fa11a87c0d0b0d8ea
kaynan98 <lucascoelho9821@gmail.com>
2026-05-02 11:19:40 -0300

    [AI] src/main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,8 @@ import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </StrictMode>,
 )
